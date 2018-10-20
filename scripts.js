82a09d4fc6930af8bf57e613628a8c7aba01bdaf
ciindii <cindythitruong@gmail.com>
2018-10-19 17:54:24 -0700

    this commit will: add random lottery number generator

--- scripts.js
+++ scripts.js
@@ -1,11 +1,47 @@
-let languages = ['javascript', 'python', 'php', 'java', 'react.js', 'node.js', 'sass'];
 
-console.log('There are ' + languages.length + ' programing languages in this list');
-console.log(languages[0] + ' is the first element from the array.');
-console.log(languages[6] + ' is the last element from the array.');
-languages.unshift('cow');
-languages.push('chicken');
-console.log(languages.join(', '));
-let x = languages.pop();
-console.log(x);
-document.write(languages);
+
+// function createRandomList() {
+//   let nums = [];
+//   for (let i = 0; 10 < i; i++) {
+//     function random100() {
+//       return Math.floor(Math.random() * 100) + 1;
+//     }
+//   } return nums;
+// }
+//
+// let myRandomList = createRandomList();
+// console.log(myRandomList);
+
+function random70() {
+  return Math.floor(Math.random() * 70) + 1;
+}
+
+function random25() {
+  return Math.floor(Math.random() * 25) + 1;
+}
+
+function createRandom5() {
+  let nums = [];
+  for (let i = 0; i < 5; i++) {
+    nums.push(random70());
+  }
+
+  return nums;
+};
+
+function createRandom1() {
+  let nums = [];
+  for (let i = 0; i < 1; i++) {
+    nums.push(random25());
+  }
+
+  return nums;
+}
+
+let num5 = createRandom5();
+let num1 = createRandom1();
+console.log('Your Mega Million numbers are: ' + num5 + ' ' + num1);
+
+// for (let i = 0; i < myRandomList.length; i++) {
+//   console.log('Item ' + i + ' in the array is ' + myRandomList[i]);
+// }
